b1016d32542ac50b71d360f3deac3a0d0b9cc03c
visnusantos1203 <visnu.santos1203@gmail.com>
2022-05-20 00:29:31 +0800

    Initial code for creating slots, parking input, and status commands

--- test.rb
+++ test.rb
@@ -1,16 +1,50 @@
 require "thor"
 require "pstore"
+require 'csv'
+require_relative 'create_slots'
+require_relative 'get_slots'
+
 class ParkingManagement < Thor
 
   desc "create_parking_lot SLOT", "creates new parking lot"
   def create_parking_lot(slots)
-    PStore.new("/tmp/parking.txt").transaction { |slot| slot[slots] = true}
+    PStore.new("/tmp/parking.txt").transaction do |slot|
+      data = []
+      slots.to_i.times do |count|
+        data << { slot_num: (count + 1), plate_num: "",  color: "", available: true }
+      end
+      slot[data] = true
+    end
     puts "Successfuly created parking with #{slots} slots"
+  end
+
+  desc "delete_parking_lot", "deletes slot"
+  def delete_parking_lot
+    PStore.new("/tmp/parking.txt").transaction { |parking| parking.delete(:last_file) }
+    puts "successfuly deleted slot number"
   end
 
   desc "park PLATE_NUM COLOR", "allocates slot to customer"
   def park(plate_num, color)
-    puts "Allocated slot number 1. Plate number: #{plate_num}, color: #{color}"
+    PStore.new("/tmp/parking.txt").transaction do |parking|
+      parking.roots.each_with_index do |park, index| # I had to do this because I created a first record with wrong format
+        unless index == 0 # and I temporarily created this to skip the first record
+          available_slot = park.find { |slot| slot[:available] == true }
+          if available_slot.nil?
+            puts "Sorry, parking lot is full"
+          else
+            available_slot[:plate_num] = plate_num
+            available_slot[:color] = color
+            available_slot[:available] = false
+
+            puts "Allocated slot number #{available_slot[:slot_num]}. Plate number: #{plate_num}, color: #{color}"
+
+          end
+        end
+      end
+
+    end
+    #puts "Allocated slot number 1. Plate number: #{plate_num}, color: #{color}"
   end
 
   desc "leave SLOT_NUM", "empties parking slot"
@@ -22,6 +56,8 @@ class ParkingManagement < Thor
   def status
     PStore.new("/tmp/parking.txt").transaction do |parking|
       parking.roots.each_with_index {|slot, index| puts "#{index + 1}. Parking #{slot}"}
+      puts "No data available" if parking.roots.empty?
+      # GetSlots.status(parking.roots)
     end
     #puts "Kailangan pa tong ayusin"
   end
